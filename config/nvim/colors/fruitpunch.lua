a657b5acfdac02e16cb50c05d7aa622b70b878dd
Coco <niraveati23@gmail.com>
2026-03-19 20:09:06 -0400

    More changes

--- config/nvim/colors/fruitpunch.lua
+++ config/nvim/colors/fruitpunch.lua
@@ -11,7 +11,7 @@ local hi = vim.api.nvim_set_hl
 -- Editor colors
 hi(0, "Normal",         { bg = "#0d0d0d", fg = "#f2f2f2" })
 hi(0, "NormalFloat",    { bg = "#191919", fg = "#f2f2f2" })
-hi(0, "CursorLine",     { bg = "#232323" })
+hi(0, "CursorLine",     { bg = "#242424" })
 hi(0, "CursorLineNr",   { fg = "#00ffff", bold = true })
 hi(0, "CursorLineFold", { bg = "NONE", fg = "#00ff00" })
 hi(0, "CursorLineSign", { bg = "NONE" })
@@ -22,23 +22,25 @@ hi(0, "IncSearch",      { fg = "#0d0d0d", bg = "#00ff00" })
 hi(0, "CurSearch",      { fg = "#0d0d0d", bg = "#00ff00" })
 hi(0, "LineNr",         { fg = "#ffff00" })
 hi(0, "EndOfBuffer",    { fg = "#262626" })
-hi(0, "NonText",        { fg = "#009900" })
 hi(0, "MatchParen",     { fg = "NONE", bg = "#404040" })
-hi(0, "Visual",         { bg = "#232323" })
+hi(0, "Visual",         { bg = "#242424" })
 hi(0, "FoldColumn",     { fg = "#00ff00", bg = "#0d0d0d" })
-hi(0, "Directory",     { fg = "#0000ff", bg = "#0d0d0d" })
+hi(0, "Directory",      { fg = "#2222ff", bg = "#0d0d0d" })
+
+-- Indent colors
+hi(0, "NonText",  { fg = "#008800" })
+hi(0, "IblScope", { fg = "#00ff00" })
 
 -- Command line
 hi(0, "ModeMsg",     { fg = "#00ffff", bg = "#0d0d0d" })
 hi(0, "MoreMsg",     { fg = "#ffff00", bg = "#0d0d0d" })
 
-
 -- Splits & borders
-hi(0, "WinSeparator",    { fg = "#f2f2f2",  bg = "NONE" })
-hi(0, "VertSplit",       { fg = "#f2f2f2",  bg = "NONE" })
+hi(0, "WinSeparator",    { fg = "#303030",  bg = "NONE" })
+hi(0, "VertSplit",       { fg = "#303030",  bg = "NONE" })
 
 -- Status & Tab Line
-hi(0, "StatusLine",  { bg = "#191919",  fg = "#aaaaaa" })
+hi(0, "StatusLine",  { bg = "#191919",  fg = "#bbbbbb" })
 hi(0, "TabLineFill", { bg = "#191919" })
 hi(0, "WinBar",      { bg = "#0d0d0d", fg = "#f2f2f2" })
 
@@ -48,7 +50,7 @@ hi(0, "Pmenu", { bg = "#191919",  fg = "#f2f2f2" })
 -- Syntax
 hi(0, "Comment",    { fg = "#5555ee" })
 hi(0, "String",     { fg = "#9933ff" })
-hi(0, "Constant",   { fg = "#0000ff" })
+hi(0, "Constant",   { fg = "#2222ff" })
 hi(0, "Variable",   { link = "Constant"})
 hi(0, "Identifier", { fg = "#00ffff" })
 hi(0, "Function",   { fg = "#ffff00" })
@@ -58,6 +60,7 @@ hi(0, "PreProc",    { fg = "#ffff00" })
 hi(0, "Special",    { fg = "#ff00ff" })
 hi(0, "Debug",      { fg = "#ff0000" })
 hi(0, "Error",      { fg = "#ff0000",  bold = true })
+hi(0, "WarningMsg",    { fg = "#ffff00" })
 hi(0, "Todo",       { fg = "#ff00ff",  bold = true })
 hi(0, "Underlined", { underline = true })
 hi(0, "Folded",     { bg = "#000033" })
@@ -67,16 +70,16 @@ hi(0, "@variable", { link = "Identifier" })
 
 -- Diagnostics (and todo-comments.nvim colors)
 hi(0, "DiagnosticError",            { fg = "#ff0000" })
-hi(0, "DiagnosticWarn",             { fg = "#ff8000" })
+hi(0, "DiagnosticWarn",             { fg = "#ffff00" })
 hi(0, "DiagnosticInfo",             { fg = "#ff00ff" })
 hi(0, "DiagnosticHint",             { fg = "#5555ee" })
 hi(0, "DiagnosticOk",               { fg = "#00ff00" })
-hi(0, "DiagnosticUnderlineError",   { undercurl = true, sp = "#ff8000" })
-hi(0, "DiagnosticUnderlineWarn",    { undercurl = true, sp = "#ff8000" })
+hi(0, "DiagnosticUnderlineError",   { undercurl = true, sp = "#ff0000" })
+hi(0, "DiagnosticUnderlineWarn",    { undercurl = true, sp = "#ffff00" })
 hi(0, "DiagnosticUnderlineInfo",    { undercurl = true, sp = "#ff00ff" })
 hi(0, "DiagnosticUnderlineHint",    { undercurl = true, sp = "#5555ee" })
 hi(0, "DiagnosticVirtualTextError", { fg = "#ff0000" })
-hi(0, "DiagnosticVirtualTextWarn",  { fg = "#ff8000" })
+hi(0, "DiagnosticVirtualTextWarn",  { fg = "#ffff00" })
 hi(0, "DiagnosticVirtualTextInfo",  { fg = "#ff00ff" })
 hi(0, "DiagnosticVirtualTextHint",  { fg = "#5555ee" })
 
@@ -98,5 +101,18 @@ hi(0, "@markup.link.label", { fg = "#0000ff" })
 hi(0, "markdownBold",       { bold = true })
 hi(0, "markdownItalic",     { italic = true })
 
+-- Heirline
+hi(0, "HeirlineNormal",   { bg = "#9933ff" })
+hi(0, "HeirlineInsert",   { bg = "#00ffff" })
+hi(0, "HeirlineVisual",   { bg = "#0000ff" })
+hi(0, "HeirlineReplace",  { bg = "#ff0000" })
+hi(0, "HeirlineCommand",  { bg = "#ff00ff" })
+hi(0, "HeirlineTerminal", { bg = "#ffff00" })
+
+-- Mason
+hi(0, "MasonHeader",             {bg = "#ffff00", fg = "#000000"})
+hi(0, "MasonHighlight",          {fg = "#00ff00" })
+hi(0, "MasonHighlightBlockBold", {bg = "#00ff00", fg = "#0d0d0d" })
+
 -- Other
-hi(0, "DashboardCenter",   { fg = "#00ff00" })
+hi(0, "DashboardCenter", { fg = "#00ff00" })
